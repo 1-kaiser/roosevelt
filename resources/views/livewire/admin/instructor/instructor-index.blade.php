78f4d2cc2e65a4776211eaf5d3d5d2afa57c42a5
zZz <emmanuelpunay6906@gmail.com>
2024-04-07 16:15:06 +0800

    py-3

--- resources/views/livewire/admin/instructor/instructor-index.blade.php
+++ resources/views/livewire/admin/instructor/instructor-index.blade.php
@@ -41,7 +41,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="p-4 bg-white overflow-hidden shadow-xl sm:rounded-lg">
 
-                <div class="flex justify-start">
+                <div class="flex justify-start py-3">
                     <livewire:admin.instructor.instructor-create />
                 </div>
                 
