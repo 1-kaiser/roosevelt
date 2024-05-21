c4ed611ad851652b5d5f78e423b1bfdb7aee59f6
zZz <emmanuelpunay6906@gmail.com>
2024-05-22 07:26:31 +0800

    customer factory

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -16,7 +16,6 @@ class CustomerFactory extends Factory
      */
     public function definition(): array
     {
-        // $customBranch = ['Maypajo Branch', 'Navotas Branch', 'Tayuman Branch', 'Fishermall Branch', 'Legarda Branch'];
 
         $phCode = +639;
         // $customCourse = ['TDC'];
@@ -25,15 +24,20 @@ class CustomerFactory extends Factory
         
         $customCourse = ['PDC'];
         $paidAttachment = ['---'];
+        $valid_id = ['---'];
         $customTransmission = ['Manual', 'Automatic'];
         
         return [
             'pic' => $this->faker->imageUrl(640, 480, 'people'),
-            'name' => $this->faker->name(),
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
             'email' => $this->faker->unique()->safeEmail(),
             'contact' => $this->faker->unique()->e164PhoneNumber($phCode),
+            'age' => $this->faker->numberBetween($min = 18, $max = 60),
+            'birthday' => $this->faker->dateTimeBetween('-65 years', '-18 years')->format('Y-m-d'),
             'date' => $this->faker->dateTimeBetween('2024-03-00', 'now'),
             'course' => $this->faker->randomElement($customCourse),
+            'valid_id' => $this->faker->randomElement($valid_id),
             'paid_attachment' => $this->faker->randomElement($paidAttachment),
             'transmission' => $this->faker->randomElement($customTransmission)
         ];
