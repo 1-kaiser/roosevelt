6ed803225836b2c9076784abaf92eacead8d1e5e
zZz <emmanuelpunay6906@gmail.com>
2024-05-21 14:12:32 +0800

    pdc confirmation

--- resources/views/mail/customer/pdc-confirmation.blade.php
+++ resources/views/mail/customer/pdc-confirmation.blade.php
@@ -3,7 +3,8 @@
 
 <p>
     Hi ka-SOLID <strong>{{ $mailData['first_name']}}</strong> ! <br /><br />
-    This is auto generated email for reservation request. Please reply <strong>YES</strong> to confirm your reservation and to notify the admin for your driving lessons on <strong>{{ $mailData['date'] }}</strong>. Reply NO if you wish to cancel your reservation. <br /> <br />
+    This is auto generated email for reservation request. Please reply <strong>YES</strong> to confirm your reservation and to notify the admin for your driving lessons on <strong>{{ $mailData['date'] }}</strong>. Reply NO if you wish to cancel your reservation. Request for enrolment cancelation and/or course downgrade is subject to company's approval. The company reserves the right to reject refund of the total course fee or any payment made upon enrolment.<br /> <br />
+    The email confirmation will have 90 days validity. <br /><br />
     If you need a quicker response, you can get in touch with us at the following numbers:
     <br /> 📞 Smart: 09998467586 / 09464583242
     <br /> 📞 Globe landline: (02) 700-56930
